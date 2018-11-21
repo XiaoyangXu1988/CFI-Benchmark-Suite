6b3409331059ba7736b2b5c399cf6907b83f8d03
XiaoyangXu1988 <44910669+XiaoyangXu1988@users.noreply.github.com>
2018-11-21 15:41:14 -0600

    Update fptr.cpp

--- fptr.cpp
+++ fptr.cpp
@@ -1,32 +1,28 @@
-#include "pch.h"
-#include <iostream>
-#include <time.h> 
-#include <windows.h>
+#include "helper.h"
 
-int loop_count = USHRT_MAX;
 static int count_even = 0;
 static int count_odd = 0;
 
-LARGE_INTEGER time_in, time_out, frequency;
-__int64 totaltime = 0;
-double elapsed_microseconds = 0.0;
+NANOSECOND start_time;
+NANOSECOND end_time;
+NANOSECOND total_time;
 
 // declare two functions with same signature
 void printEven(int n);
 void printOdd(int n);
 
 int main()
-{	
+{
 	// declare a function pointer, so that each call 
 	// via this pointer would be a indirect call
-	void(*fptr) (int n);	
+	void(*fptr) (int n);
 
 	// initialize random seed
 	srand((unsigned int)time(NULL));
 	// record starting time
-	QueryPerformanceCounter(&time_in);
-	
-	for (int i = 0; i < loop_count; i++)
+	start_time = get_wall_time();
+
+	for (int i = 0; i < MAX_lOOP; i++)
 	{
 		// depend on parity of randomized number, call
 		// different function using function pointer
@@ -39,13 +35,11 @@ int main()
 	}
 
 	// record ending time, and calculate running time elapsed in the loop
-	QueryPerformanceCounter(&time_out);
-	QueryPerformanceFrequency(&frequency);
-	totaltime += time_out.QuadPart - time_in.QuadPart;
-	elapsed_microseconds = (double)totaltime * 1000000 / frequency.QuadPart;
+	end_time = get_wall_time();
+	total_time = end_time - start_time;
 
 	// print results
-	printf("total time in microseconds is %f\n", elapsed_microseconds);
+	printf("total time in nanoseconds is %llu\n", (long long unsigned int) total_time);
 
 	printf("%d odd numbers\n", count_odd);
 	printf("%d even numbers\n", count_even);
