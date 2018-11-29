e8bc8e27e71fea5503536b84403fe6e0b7a3a3f9
XiaoyangXu1988 <44910669+XiaoyangXu1988@users.noreply.github.com>
2018-11-29 16:51:55 -0600

    Update IATcall.cpp

--- IATcall.cpp
+++ IATcall.cpp
@@ -1,36 +1,67 @@
 #include "helper.h"
 
-static int count_even = 0;
-static int count_odd = 0;
-
 NANOSECOND start_time;
 NANOSECOND end_time;
 NANOSECOND total_time;
+const int loop_count = 10000L;
 
 int main()
 {
-	// declare a function pointer, so that each call 
-	// via this pointer would be a indirect call
-	void(*fptr) (int n);
+	LPVOID base_addr = NULL;
+	DWORD dwOldProtect;
+	BOOL retVP = FALSE;
+	BOOL retVF = FALSE;
+	SIZE_T dwSize = 1;
 
-	// initialize random seed
-	srand((unsigned int)time(NULL));
 	// record starting time
-	start_time = get_wall_time();
+	start_time = get_wall_time();	
 
-	// Activity to be timed
-	for (int i = 0; i < MAX_lOOP; i++)
+	for (int i = 0; i < loop_count; ++i)
 	{
-		// printf is a IAT function call.
-		printf("This is a message between start_time and end_time\n");
+		base_addr = VirtualAlloc(
+			NULL,
+			dwSize,
+			MEM_COMMIT,
+			PAGE_READWRITE
+		);
+		
+		if (!base_addr)
+		{
+			printf("VirtualAlloc() failed...\n");
+			exit(1);
+		}
+
+		retVP = VirtualProtect(
+			base_addr,
+			dwSize,
+			PAGE_READONLY,
+			&dwOldProtect
+		);
+
+		if (!retVP)
+		{
+			printf("VirtualProtect() failed...\n");
+			exit(1);
+		}
+
+		retVF = VirtualFree(
+			base_addr,
+			0,
+			MEM_RELEASE
+		);
+
+		if (!retVF)
+		{
+			printf("VirtualFree() failed...\n");
+			exit(1);
+		}
 	}
 
-	// record ending time, and calculate running time elapsed in the loop
+	// record ending time
 	end_time = get_wall_time();
 	total_time = end_time - start_time;
 
 	// print results
 	printf("total time in nanoseconds is %llu\n", (long long unsigned int) total_time);
-	//getchar();
 	return 0;
 }
