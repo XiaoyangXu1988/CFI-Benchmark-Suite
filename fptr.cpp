43680015e6fb9401864be614bf950a74dd8dc55c
XiaoyangXu1988 <44910669+XiaoyangXu1988@users.noreply.github.com>
2018-11-30 16:50:33 -0600

    Update fptr.cpp

--- fptr.cpp
+++ fptr.cpp
@@ -22,7 +22,7 @@ int main()
 	// record starting time
 	start_time = get_wall_time();
 
-	for (int i = 0; i < MAX_lOOP; i++)
+	for (int i = 0; i < MAX_LOOP; i++)
 	{
 		// depend on parity of randomized number, call
 		// different function using function pointer
