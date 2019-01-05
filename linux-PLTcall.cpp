c6a001b508b18210aa24f3f0e427d878c5a50b84
Masoud Ghaffarinia <ghaffarinia.masoud@gmail.com>
2019-01-05 15:03:11 -0600

    adding PLTcall benchmark for linux quite the same as IAT for windows

---
+++ linux-PLTcall.cpp
@@ -0,0 +1,47 @@
+#include "helper.h"
+
+NANOSECOND start_time;
+NANOSECOND end_time;
+NANOSECOND total_time;
+const int loop_count = 10000L;
+
+int main()
+{
+    char *p;
+    int page_size;
+    int fd;
+    
+	// record starting time
+	start_time = get_wall_time();	
+
+	for (int i = 0; i < loop_count; ++i)
+	{
+        
+        page_size = getpagesize();
+        fd = open ("/dev/zero", O_RDONLY);
+        p = (char *) mmap (NULL, page_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+        close (fd);
+        
+        if (!p) {
+            perror("Couldn’t malloc(1024)");
+            exit(errno);
+        }
+        
+        p[123] = 123;
+        
+        if (mprotect(p, page_size, PROT_NONE)) {
+            perror("Couldn’t mprotect");
+            exit(errno);
+        }
+        
+        munmap(p, page_size);
+	}
+
+	// record ending time
+	end_time = get_wall_time();
+	total_time = end_time - start_time;
+
+	// print results
+	printf("total time in nanoseconds is %llu\n", (long long unsigned int) total_time);
+	return 0;
+}
