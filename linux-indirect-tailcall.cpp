391fd665006ed30db0ce504f1a789ca0e566a339
Masoud Ghaffarinia <ghaffarinia.masoud@gmail.com>
2019-01-23 17:41:20 -0600

    making indirect tail call work for both 32 bit and 64 bit as well as windows and linux

--- linux-indirect-tailcall.cpp
+++ linux-indirect-tailcall.cpp
@@ -1,49 +1,75 @@
 #include "helper.h"
 
-static int count_even = 0;
-static int count_odd = 0;
+static int zero_count = 0;
+static int one_count = 0;
+static int two_count = 0;
+static int three_count = 0;
+const int divisor = 4;
 
-void randAndCount();
-void countEven(int n);
-void countOdd(int n);
+NANOSECOND start_time;
+NANOSECOND end_time;
+NANOSECOND total_time;
+
+void helper();
+void case0();
+void case1();
+void case2();
+void case3();
 
 int main()
 {
-	srand((unsigned int)time(NULL));	
-	for (int i = 0; i < MAX_LOOP; i++)
-	{
-		randAndCount();
-	}
+srand((unsigned int)time(NULL));
+start_time = get_wall_time();
 
-	printf("%d odd numbers\n", count_odd);
-	printf("%d even numbers\n", count_even);
-
-	return 0;
+for (int i = 0; i < MAX_LOOP; i++)
+{
+helper();
 }
 
-void randAndCount()
-{
-	void(*fptr) (int n);
-	int r = rand();
+end_time = get_wall_time();
+total_time = end_time - start_time;
 
-	if (r % 2 == 0)
-	{
-		fptr = countEven;
-	}
-	else
-	{
-		fptr = countOdd;
-	}
-
-	fptr(r);
+printf("total time in nanoseconds is %llu\n", (long long unsigned int) total_time);
+printf("%d numbers have remainder of zero modulo %d.\n", zero_count, divisor);
+printf("%d numbers have remainder of one modulo %d.\n", one_count, divisor);
+printf("%d numbers have remainder of two modulo %d.\n", two_count, divisor);
+printf("%d numbers have remainder of three modulo %d.\n", three_count, divisor);
 }
 
-void  countEven(int n)
+void helper()
 {
-	count_even++;
+void(*fptr) ();
+int r = rand();
+int remainder = r % divisor;
+
+if (remainder == 0)
+fptr = case0;
+if (remainder == 1)
+fptr = case1;
+if (remainder == 2)
+fptr = case2;
+if (remainder == 3)
+fptr = case3;
+
+fptr();
 }
 
-void countOdd(int n)
+void case0()
 {
-	count_odd++;
+zero_count += 1;
+}
+
+void case1()
+{
+one_count += 1;
+}
+
+void case2()
+{
+two_count += 1;
+}
+
+void case3()
+{
+three_count += 1;
 }
