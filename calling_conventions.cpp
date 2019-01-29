b3b5f2edbe368d41d7fff2c684d472a1f0f0bafb
XiaoyangXu1988 <44910669+XiaoyangXu1988@users.noreply.github.com>
2019-01-29 13:00:51 -0600

    Update calling_conventions.cpp
    make it gcc compatible

--- calling_conventions.cpp
+++ calling_conventions.cpp
@@ -1,16 +1,16 @@
 #include "helper.h"
 
 // caller-pop, variadic function
-int __cdecl variadicSum(int count, ...);
+int CDECL variadicSum(int count, ...);
 
 // callee-pop function
-int __stdcall listSum(int count, const int * list);
+int STDCALL listSum(int count, const int * list);
 
-// callee-pop __fastcall function. caller tries to pass 
+// callee-pop FASTCALL function. caller tries to pass 
 // argument(s) using registers first.
-int __fastcall fastSum(int count, const int * list);
+int FASTCALL fastSum(int count, const int * list);
 
-// callee-pop __thiscall functions. "this" pointer is passed 
+// callee-pop THISCALL functions. "this" pointer is passed 
 // to callee via ecx. other arguments are pushed onto stack
 class CMath
 {
@@ -18,34 +18,34 @@ private:
 	int * list;
 	int count;
 public:
-	int __thiscall calculateSum();
-	int __thiscall calculateAverage();
-	void __thiscall setList(const int list_count, const int * int_list);
+	int THISCALL calculateSum();
+	int THISCALL calculateAverage();
+	void THISCALL setList(const int list_count, const int * int_list);
 };
 
 int main()
 {
-	// call a __cdecl function
+	// call a CDECL function
 	int variadicSum_ans = 0;
 
 	variadicSum_ans = variadicSum(5, 1, 2, 3, 4, 5);
 	printf("variadicSum: sum is %d\n", variadicSum_ans);
 
-	// call a __stdcall function
+	// call a STDCALL function
 	int listSum_ans = 0;
 	int int_list1[5] = { 1, 3, 5, 7, 9 };
 
-	listSum_ans = listSum(sizeof(int_list1)/sizeof(int), int_list1);
+	listSum_ans = listSum(sizeof(int_list1) / sizeof(int), int_list1);
 	printf("listSum: sum is %d\n", listSum_ans);
 
-	// call a __fastcall function
+	// call a FASTCALL function
 	int fastSum_ans = 0;
 	int int_list2[4] = { 5, 3, 5, 1 };
 
 	fastSum_ans = fastSum(sizeof(int_list2) / sizeof(int), int_list2);
 	printf("fastSum: sum is %d\n", fastSum_ans);
 
-	// call __thiscall functions
+	// call THISCALL functions
 	CMath * myCMath = new CMath;
 	int int_list3[6] = { 2, 1, 6, 3, 8, 9 };
 
@@ -58,7 +58,7 @@ int main()
 	return 0;
 }
 
-int __thiscall CMath::calculateAverage()
+int THISCALL CMath::calculateAverage()
 {
 	int sum = 0;
 	int avg = 0;
@@ -68,7 +68,7 @@ int __thiscall CMath::calculateAverage()
 	return avg;
 }
 
-int __thiscall CMath::calculateSum()
+int THISCALL CMath::calculateSum()
 {
 	int sum = 0;
 	for (int i = 0; i < this->count; i++)
@@ -77,7 +77,7 @@ int __thiscall CMath::calculateSum()
 	return sum;
 }
 
-void __thiscall CMath::setList(const int list_count, const int * int_list)
+void THISCALL CMath::setList(const int list_count, const int * int_list)
 {
 	this->list = new int[list_count];
 	this->count = list_count;
@@ -86,7 +86,7 @@ void __thiscall CMath::setList(const int list_count, const int * int_list)
 		(this->list)[i] = int_list[i];
 }
 
-int __fastcall fastSum(int count, const int * list)
+int FASTCALL fastSum(int count, const int * list)
 {
 	int sum = 0;
 	int i = 0;
@@ -97,7 +97,7 @@ int __fastcall fastSum(int count, const int * list)
 	return sum;
 }
 
-int __stdcall listSum(int count, const int * list)
+int STDCALL listSum(int count, const int * list)
 {
 	int sum = 0;
 	int i = 0;
@@ -108,7 +108,7 @@ int __stdcall listSum(int count, const int * list)
 	return sum;
 }
 
-int __cdecl variadicSum(int count, ...)
+int CDECL variadicSum(int count, ...)
 {
 	va_list ap;
 	int sum = 0;
