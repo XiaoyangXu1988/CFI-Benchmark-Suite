1487bc45d62a3230dac3ae8de2e9588f7159ab6a
XiaoyangXu1988 <44910669+XiaoyangXu1988@users.noreply.github.com>
2019-01-29 12:59:55 -0600

    Update helper.h

--- helper.h
+++ helper.h
@@ -9,31 +9,29 @@
 #elif __linux__
 #include <iostream>
 #include <time.h>
-#include <stdio.h>
-#include <stdlib.h> 
+#include <stdio.h> 
 #include <limits.h>
 #include <stdint.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <dlfcn.h>
-#include <iostream>
-#include <sys/types.h>
-#include <pthread.h>
 #else
 //
 #endif
 
-#ifndef FALSE
-#define FALSE false
-#endif
-
-//#define MAX_LOOP INT_MAX
-#define MAX_LOOP 100
+#define MAX_LOOP 1024
 #define NANOSECOND uint64_t
 #define BILLION 1000000000L
 
 extern NANOSECOND get_wall_time();
 
-
-
+#ifdef _WIN32
+#define CDECL __cdecl
+#define STDCALL __stdcall
+#define FASTCALL __fastcall
+#define THISCALL __thiscall
+#elif __linux__
+#define CDECL __attribute__((cdecl))
+#define STDCALL __attribute__((stdcall))
+#define FASTCALL __attribute__((fastcall))
+#define THISCALL __attribute__((thiscall))
+#else
+//
+#endif
